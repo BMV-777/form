2c9e8376153fcbc13ea1ce5c6a906a7f0c74e72e
Maks Borenkov <borenkovmv@gmail.com>
2022-12-02 14:38:43 +0200

    12

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { Component } from 'react';
 import './App.css';
-import { Form } from 'component/Form/Form';
+import { LoginForm } from './component/LoginForm/LoginForm.jsx';
+// import { Form } from 'component/Form/Form';
 // import { hasFormSubmit } from '@testing-library/user-event/dist/utils';
 
 class App extends Component {
-  state = {};
+  // state = {};
 
-  hasFormSubmitHantler = data => {
-    console.log(data);
-  };
+  // hasFormSubmitHantler = data => {
+  //   console.log(data);
+  // };
 
   render() {
     return (
       <div>
-        <Form onSubmit={this.hasFormSubmitHantler} />
+        <LoginForm />
+        {/* <Form onSubmit={this.hasFormSubmitHantler} /> */}
       </div>
     );
   }
---
+++ src/component/LoginForm/LoginForm.jsx
@@ -0,0 +1,56 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import styled from 'styled-components';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  login: yup.string().required(),
+  password: yup.string().min(4).max(8).required(),
+});
+
+const Error = styled.p`
+  color: blue;
+`;
+const Input = styled(Field)`
+  color: red;
+`;
+
+const initialValues = {
+  login: '',
+  password: '',
+};
+
+export const LoginForm = () => {
+  // const handler = e => {
+  //   e.preventDefault();
+  //   const { login, password } = e.target.elements;
+  //   console.log(login.value, password.value);
+  // };
+
+  const handler = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={handler}
+    >
+      <Form autoComplete="off">
+        <label htmlFor="login">
+          Login
+          <Input type="text" name="login" />
+          <ErrorMessage component={Error} name="login" />
+        </label>
+
+        <label htmlFor="password">
+          Password
+          <Input type="text" name="password" />
+          <ErrorMessage component={Error} name="password" />
+        </label>
+        <button type="submit">Summit</button>
+      </Form>
+    </Formik>
+  );
+};
